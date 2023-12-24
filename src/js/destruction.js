501f68c5637ee46c13d60602627ae0a7b802287e
Viktor Kayatkin <kayatkin@mail.ru>
2023-12-25 01:24:11 +0700

    initial commit

---
+++ src/js/destruction.js
@@ -0,0 +1,14 @@
+export default function showOptionsAttacks(obj) {
+  if (!("special" in obj)) {
+    throw new Error("Нет доступных вариантов спец.атак для этого персонажа");
+  }
+
+  return obj.special.map(
+    ({ id, name, icon, description = "Описание недоступно" }) => ({
+      id,
+      name,
+      icon,
+      description,
+    })
+  );
+}
